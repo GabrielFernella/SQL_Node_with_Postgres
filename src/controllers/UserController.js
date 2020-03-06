9591662b7cdbb921b1e1678eef900baa009af7a5
Gabriel Fernella Garcia dos Santos <Gabrielfernella@users.noreply.github.com>
2020-03-06 16:53:24 -0300

    Create Models and Controllers
    create models and Controllers -> Insert Users

---
+++ src/controllers/UserController.js
@@ -0,0 +1,14 @@
+const User = require('../models/User');
+
+module.exports = {
+    async store(req,res){
+        const { name, email} = req.body;
+
+        const user = await User.create({
+            name,
+            email,
+        });
+
+        return res.json(user);
+    }
+};
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
+const User = require('../models/User');
+
 const connection = new Sequelize(dbConfig); //conexão com o banco
+
+User.init(connection);
 
 module.exports = connection;
