00dd882d2695d2bc122234e6bd6d11b1a7e018ca
Gabriel Fernella Garcia dos Santos <Gabrielfernella@users.noreply.github.com>
2020-03-06 16:25:23 -0300

    First Migration to database
    First migration to database, create table users

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,11 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
+    host: '192.168.99.100',
     username: 'postgres',
     password: 'docker',
-    database: 'sqlnode',
+    database: 'postdata',
     define: {
         timestamps: true,
         underscored: true,
-        
     },
-}
+};
